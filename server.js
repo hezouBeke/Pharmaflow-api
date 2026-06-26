d71aa4d749605ddb483a27663bde5483977d56f0
hezouBeke <hezoubeke18@gmail.com>
2026-06-26 23:05:15 +0000

    server config

---
+++ server.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+
+app.use(express.json());
+
+
+
+mongoose.connect(process.env.MONGO_URI)
+
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => console.log(err));
+
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, () => (console.log(`Server is running on port ${PORT}`)));
