d6d1618c0c5e860df3e823ed9d8e9a04a2f760f7
hezouBeke <hezoubeke18@gmail.com>
2026-07-03 22:58:32 +0000

    feat : remove password for client

--- models/User.js
+++ models/User.js
@@ -1,11 +1,10 @@
-const mongooe = require('mongoose');
+const mongoose = require('mongoose');
 
 
 const userSchema = new mongoose.Schema({
     name : { type : String, required : true },
     sex : { type : String, required : true },
     email : { type : String, required : true, unique : true },
-    password : { type : String, required : true , unique : true },
     age :{ type : Number, required : true },
     tel : { type : String, required : true },
 
