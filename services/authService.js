ef4df53b0b656ccd0023fc143c50808f79c5ea7e
hezouBeke <hezoubeke18@gmail.com>
2026-07-09 15:31:57 +0000

    feat : login service

--- services/authService.js
+++ services/authService.js
@@ -10,3 +10,15 @@ exports.registerUser = async (userData) => {
     await user.save();
     return user;
 };
+
+exports.loginUser = async (email, password) => {
+    const user = await User.findOne({ email });
+
+    if(!user)  throw new Error("Email incorrecte");
+
+    const isMatch = await bcrtypt.compare(password, user.password);
+    if(!isMatch) throw new Error("Mot de passe incorrecte");
+
+    const token = jwt.sign({ id: user._id, role: user.role}, process.env.JWT_SECRET, { expiresIn: "1d" });
+    return token;
+};
