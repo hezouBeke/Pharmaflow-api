96a9a11efe79fd5660a745f3553fbff5c87ab49b
hezouBeke <hezoubeke18@gmail.com>
2026-08-01 21:46:43 +0000

    add password to userschema

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
     name : { type : String, required : true },
     sex : { type : String, required : true },
     email : { type : String, required : true, unique : true },
+    password : { type : String , required : true },
     age :{ type : Number, required : true },
     tel : { type : String, required : true },
 
