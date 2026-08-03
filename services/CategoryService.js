a37debfb26d4caf9d0f40a21ba37cfae63ca745c
hezouBeke <hezoubeke18@gmail.com>
2026-08-03 22:30:32 +0000

    create cat service

---
+++ services/CategoryService.js
@@ -0,0 +1,7 @@
+ const Category = require("../models/Category");
+
+ exports.createCategory = async (categoryData) => {
+    const category = new Category({ ...categoryData });
+    await category.save();
+    return category;
+ };
