b5418007d9de04b6d10a0510d5b392c64e27f0d1
hezouBeke <hezoubeke18@gmail.com>
2026-08-03 22:56:51 +0000

    cat route

---
+++ routes/CatRoute.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const authorize = require('../middleware/authorize');
+
+router.get("/", CategoryController.get);
+router.get("/:id", CategoryController.getOne);
+
+router.post("/", authorize(["admin"]), CategoryController.create);
+router.put("/:id", authorize(["admin"]), CategoryController.update);
+router.delete("/:id", authorize(["admin"]), CategoryController.delete);
+module.exports = router;
