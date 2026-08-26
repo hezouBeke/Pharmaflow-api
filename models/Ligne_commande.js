98c7777e90bcb5838628d1a0639fcb0beb6d1ab5
hezouBeke <hezoubeke18@gmail.com>
2026-08-26 22:01:14 +0000

    feat: add LigneCommande model

---
+++ models/Ligne_commande.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const LignSchema = new mongoose.Schema({ 
+    vente: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Vente',
+        required: true,
+    },
+    medicament: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Medicament',
+        required: true,
+    },
+    quantite: {
+        type: Number,
+        required: true,
+    },
+    prixUnitaire: {
+        type: Number,
+        required: true,
+    },
+   
+},{
+    timestamps: true
+});
+
+module.exports = mongoose.model("Ligne_commande", LignSchema);
