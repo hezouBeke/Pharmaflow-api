b9ddf83668911f329872aada21f60401aa274dd0
hezouBeke <hezoubeke18@gmail.com>
2026-09-04 23:31:34 +0000

    feat: add Vente service with automatic stock and total calculation

--- controllers/VenteController.js
+++ controllers/VenteController.js
@@ -4,9 +4,10 @@ const venteService = require('../services/VenteService');
 
 exports.create = async (req, res) => {
     try{
-        const vente = await venteService.createVente(req.body);
+        const vente = await venteService.createVente(req.body, req.user.id);
         res.json({ message: "Vente ajoutée avec succès ✅", vente });
     } catch (error) {
+        console.error("Erreur lors de l'ajout de la vente :", error);
         res.status(500).json({ message: "Erreur lors de l'ajout de la vente", error });
     };
 };
